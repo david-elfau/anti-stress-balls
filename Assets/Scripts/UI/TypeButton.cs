ba106da3ef1605cd44014ef31bc7d726fa69e079
david-elfau <david.elfau@gmail.com>
2021-12-28 22:58:49 +0100

    Add overlay shader to type button

--- Assets/Scripts/UI/TypeButton.cs
+++ Assets/Scripts/UI/TypeButton.cs
@@ -2,20 +2,37 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TypeButton : IButton
 {
     public BallType BallType;
+    [SerializeField] private Material SpriteOutlineMaterialON;
+    [SerializeField] private Material SpriteOutlineMaterialOFF;
 
     public void Initizialize(BallType ballType)
     {
         BallType = ballType;
         UpdateButtonText(BallType.TypeName);
+
+        ChangeMaterial();
     }
 
 
     public override void OnButtonTap()
     {
         EventBus.Instance.TriggerEvent(EventName.ChangeType, new ParameterBusObject(BallType.TypeName));
+    }
+
+    private void ChangeMaterial()
+    {
+        if (IsSelected)
+        {
+            gameObject.GetComponent<Image>().material = SpriteOutlineMaterialON;
+        }
+        else
+        {
+            gameObject.GetComponent<Image>().material = SpriteOutlineMaterialOFF;
+        }
     }
 }
